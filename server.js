594854c0f08f7e71ed839ca6e1677c06fd8e3329
victor-villafane <victor.villafane@davinci.edu.ar>
2024-11-20 15:52:24 -0300

    se agrego la posibilidad de subir imagenes y socket

--- server.js
+++ server.js
@@ -3,12 +3,57 @@ import cors from "cors"
 import ProductoRoute from "./routes/productos.routes.js"
 import ApiRoute from "./api/routes/productos.routes.js"
 import ApiUsuario from "./api/routes/usuarios.routes.js"
+import multer from "multer"
+import sharp from "sharp"
+import http from "http"
+import { Server as SocketIO } from "socket.io"
+
 const app = express()
+const server = http.createServer(app)
+
+const io = new SocketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", () => {
+    console.log("conectado!")
+})
+
+io.on("mensaje", (dato) => {
+    console.log("mensaje")
+})
 
 const corsOptions = {
     origin: "http://localhost:5173",
     methods: "GET, POST"
 }
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, "./uploads")
+    },
+    filename: function(req, file, cb){
+        cb( null, file.originalname.trim().replace(/\s/g, "_") )
+    }
+})
+
+async function resizeImage( req, res, next ){
+    return sharp( req.file.path )
+        .resize(150)
+        .webp()
+        .rotate(90)
+        .toFile("uploads/" + ( new Date().getTime() ) + ".webp")
+        .then( () => {
+            console.log("Imagen redimencionada")
+            next()
+        } )
+        .catch( err => res.status(500).json({"error": err}) )
+}
+
+const upload = multer({ storage: storage })
 
 app.use( express.urlencoded({ extended: true }) )
 app.use( express.json() )
@@ -18,7 +63,12 @@ app.use("/api",ApiUsuario)
 
 app.use(ProductoRoute)
 
-app.listen(2024, () => console.log("Ready"))
+app.post( "/upload",[upload.single("file"), resizeImage], (req, res) => {
+    console.log(req.file, req.body)
+    res.send("ok")
+} )
+
+server.listen(2024, () => console.log("Ready"))
 
 /**
  * 1. La url no hace referencia a la locacion, sino identifica un recurso.
